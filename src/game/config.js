188adbd6a624db71d2f00c377e0bc8a84cd013d3
Mason Lee <kingsinland@gmail.com>
2022-07-28 00:13:49 -0500

    SNap

--- src/game/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  maxHealth: 100,
-  mapScale: 0.5
-};
--- src/scenes/index.js
+++
@@ -1,4 +0,0 @@
-import { Tutorial } from "./tutorial";
-import { Level1 } from "./level1";
-import BootScene from "./boot";
-export { Tutorial, Level1, BootScene };
