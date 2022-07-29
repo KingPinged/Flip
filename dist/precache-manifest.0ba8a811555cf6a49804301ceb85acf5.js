ab5317962711f8a5bf11814711854da34b2c1596
Mason L <kingsinland@gmail.com>
2022-07-29 14:34:10 -0500

    dist folder required for vercel

---
+++ dist/precache-manifest.0ba8a811555cf6a49804301ceb85acf5.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d89a8f0d3544ad56fe68bf6518ff5f",
+    "url": "assets/ammo/kripken/ammo.js"
+  },
+  {
+    "revision": "62f3fccdacecb20f93c6198f491d2d22",
+    "url": "assets/ammo/kripken/ammo.wasm.js"
+  },
+  {
+    "revision": "24cec114e4e97fc60bf62e9b51433978",
+    "url": "assets/ammo/kripken/ammo.wasm.wasm"
+  },
+  {
+    "revision": "c6465e4fc058915dd6da531aa722bebc",
+    "url": "assets/ammo/moz/ammo.js"
+  },
+  {
+    "revision": "b9fad0a49e64f4fa99fe43aa1fb85de3",
+    "url": "assets/ammo/moz/ammo.wasm.js"
+  },
+  {
+    "revision": "8640fe8d5bf91d2e84ea56e0c1307ba9",
+    "url": "assets/ammo/moz/ammo.wasm.wasm"
+  },
+  {
+    "revision": "5456a698b4c83c57d49c102234a67b08",
+    "url": "assets/atlas/ninja-texture-atlas.json"
+  },
+  {
+    "revision": "163fd1e77858c0f453bfb662680a2ae7",
+    "url": "assets/atlas/ninja-texture-atlas.png"
+  },
+  {
+    "revision": "2b6791005aa622a8236da1d07acca13c",
+    "url": "assets/button/button_sprite_sheet.png"
+  },
+  {
+    "revision": "45a70fd08e5b0689f29e4557bfe44722",
+    "url": "assets/fbx/BodyJabCross.fbx"
+  },
+  {
+    "revision": "764ea9e9be3adbe56f3173196b0656ca",
+    "url": "assets/fbx/HipHopDancing.fbx"
+  },
+  {
+    "revision": "8a275bd498ff7f760ec436f243ba9e97",
+    "url": "assets/fbx/Idle.fbx"
+  },
+  {
+    "revision": "20b5b7461df04d8d6b9402c1fdc30ca0",
+    "url": "assets/fbx/Jumping.fbx"
+  },
+  {
+    "revision": "5893fcba142fe5dd013b350a319dd497",
+    "url": "assets/fbx/LookingAround.fbx"
+  },
+  {
+    "revision": "a9a73f19ed4570ca5eb2f23e336b99de",
+    "url": "assets/fbx/Running.fbx"
+  },
+  {
+    "revision": "04ef3aa72f48b42aa337f949556bc1fe",
+    "url": "assets/fbx/Walking.fbx"
+  },
+  {
+    "revision": "cf6b1c5e0b67500c2719e8c47b0e0493",
+    "url": "assets/glb/Duck.glb"
+  },
+  {
+    "revision": "58ad7883cffb76ad8cfbc35ae5f46279",
+    "url": "assets/glb/M4A1.glb"
+  },
+  {
+    "revision": "7c1fb70e85bb6e1c53379c680220f682",
+    "url": "assets/glb/book.glb"
+  },
+  {
+    "revision": "a711da6dbb7b0ae8b6e74bda67fb1e6e",
+    "url": "assets/glb/box_man.glb"
+  },
+  {
+    "revision": "91801da7cea35836223305a18851b55a",
+    "url": "assets/glb/car.glb"
+  },
+  {
+    "revision": "70610664823701a3341a6e3db0af2fa7",
+    "url": "assets/glb/robot.glb"
+  },
+  {
+    "revision": "40cc9e582054eca1e6b27b37b90d7315",
+    "url": "assets/glb/suzanne.glb"
+  },
+  {
+    "revision": "e34de2ea2c18cd6b630798110481aba9",
+    "url": "assets/glb/suzannemorphed.glb"
+  },
+  {
+    "revision": "9efdcacbe1eb0875b912369b0fc676ba",
+    "url": "assets/gltf/drone/drone.bin"
+  },
+  {
+    "revision": "24f6421bf1116c4686b0b70214a16573",
+    "url": "assets/gltf/drone/drone.gltf"
+  },
+  {
+    "revision": "fe90699712510a5c0c760ed441928049",
+    "url": "assets/gltf/drone/textures/1001_baseColor.png"
+  },
+  {
+    "revision": "6041acc3b083bb919a9797fe501de657",
+    "url": "assets/gltf/drone/textures/1001_metallicRoughness.png"
+  },
+  {
+    "revision": "53c3c85b406e9ac6ff049861594d7eba",
+    "url": "assets/gltf/drone/textures/1001_normal.png"
+  },
+  {
+    "revision": "0d075771db3a60298c15b1ab2257bf31",
+    "url": "assets/gltf/drone/textures/ai_whitePlastic_tex_baseColor.png"
+  },
+  {
+    "revision": "3193fcd17892f974eec63b6b13fba387",
+    "url": "assets/heightmap/heightmap-island.png"
+  },
+  {
+    "revision": "138b199dfb404a2a82f087cc87cd0c99",
+    "url": "assets/heightmap/heightmap-simple.png"
+  },
+  {
+    "revision": "c5827a6394eece3185f28e75890c72a8",
+    "url": "assets/img/box-bottom.png"
+  },
+  {
+    "revision": "0c2335c06a6eda1025632736ade9015e",
+    "url": "assets/img/box-side.png"
+  },
+  {
+    "revision": "22117b222de33cd888a6ee5d368207a6",
+    "url": "assets/img/box-top.png"
+  },
+  {
+    "revision": "801373c211724f955fb17e1e2b5ba453",
+    "url": "assets/img/grass.jpg"
+  },
+  {
+    "revision": "f00a8e477865e092cf837b0dd1490fba",
+    "url": "assets/img/grass.md"
+  },
+  {
+    "revision": "34401589a72c5aff231eadd64bbdc958",
+    "url": "assets/img/grid.png"
+  },
+  {
+    "revision": "cb8fe21064e648d4fd4aa71e2a5d339d",
+    "url": "assets/img/sky.png"
+  },
+  {
+    "revision": "1cedadfa4f29f30af88bd82336a560b0",
+    "url": "assets/physics/fruit-shapes.json"
+  },
+  {
+    "revision": "20c65cf71aacabf303daf378fa05adf4",
+    "url": "assets/physics/fruit-sprites.json"
+  },
+  {
+    "revision": "72af645dad9bbd05fcc2387920916f9a",
+    "url": "assets/physics/fruit-sprites.png"
+  },
+  {
+    "revision": "a008dfe798572f75edc577f9678bdd04",
+    "url": "assets/spritesheet/adventurer-Sheet.png"
+  },
+  {
+    "revision": "4db84e856e6a3cc02600e27c40ea86db",
+    "url": "assets/svg/bridge.svg"
+  },
+  {
+    "revision": "784298ed64060c18aa884ec7748d9682",
+    "url": "assets/svg/star.svg"
+  },
+  {
+    "revision": "a33d50da063b016852d1d139cf6e73b1",
+    "url": "assets/water/Water_1_M_Normal.jpg"
+  },
+  {
+    "revision": "639428cf065384aae22d01b529011992",
+    "url": "assets/water/Water_2_M_Normal.jpg"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "2ffbc23293ee8a797bc61e9c02534206",
+    "url": "icons/icons-192.png"
+  },
+  {
+    "revision": "8bdcc486cda9b423f50e886f2ddb6604",
+    "url": "icons/icons-512.png"
+  },
+  {
+    "revision": "52126db3b195defaded1d9a9e4d82732",
+    "url": "index.html"
+  },
+  {
+    "revision": "729a36f6200b88df2da2",
+    "url": "main.dcf758fa141dcb83ec9d.bundle.js"
+  },
+  {
+    "revision": "bf38d0c9760162342f12cd9b44fbf4ef",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "sw.js"
+  },
+  {
+    "revision": "b089f144172711d5f352",
+    "url": "vendors.91ba28ac32e14536a246.bundle.js"
+  }
+]);
